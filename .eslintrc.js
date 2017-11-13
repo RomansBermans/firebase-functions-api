81ed6c9a7b4cbeb09f30f82508197b036a3d134b
Romans Bermans <romans.bermans@gmail.com>
2017-11-13 15:09:15 +0100

    Updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
   extends: 'airbnb-base',
   rules: {
     'arrow-parens': ['error', 'as-needed'],
     'consistent-return': 'off',
     'function-paren-newline': 'off',
-    'max-len': [ 'error', 240 ],
     'newline-per-chained-call': 'off',
   }
 }
